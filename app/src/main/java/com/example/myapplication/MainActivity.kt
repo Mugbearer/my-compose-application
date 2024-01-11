22a521cff09b8c02bde015adcd262e28824b3582
Hobey Padlan <hobeypadlanxx@gmail.com>
2024-01-11 21:54:25 +0800

    applied background img

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +29,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Perrell","Brown")
+                    GreetingImage("Perrell","Brown")
                 }
             }
         }
@@ -37,33 +38,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            modifier = modifier.padding(8.dp)
-        ) {
-            Text(
-                text = "Hello, my name is $name!",
-                fontSize = 100.sp,
-                lineHeight = 116.sp,
-                textAlign = TextAlign.Center
-                //modifier = modifier.padding(5.dp)
-            )
-            Text(
-                text = "From $from",
-                fontSize = 36.sp,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(alignment = Alignment.End)
-            )
-        }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(
+            text = "Hello, my name is $name!",
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+            //modifier = modifier.padding(5.dp)
+        )
+        Text(
+            text = "From $from",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Greeting(
+            name = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Perrell","Brown")
+        GreetingImage("Perrell","Brown")
     }
 }
